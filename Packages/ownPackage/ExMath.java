9ef0bb1d0974c96df86b09e3b4ff07fff0750f01
Phani4T <knowntheunknown07@gmail.com>
2023-04-29 11:11:17 +0530

    hi

---
+++ Packages/ownPackage/ExMath.java
@@ -0,0 +1,20 @@
+package ownPackage;
+public class ExMath
+{
+	public float add(int a,int b)
+	{
+		return (a+b);
+	}
+	public float sub(int a,int b)
+	{
+		return (a-b);
+	}
+	public float mul(float a,float b)
+	{
+		return (a*b);
+	}
+	public float div(float a,float b)
+	{
+		return (a/b);
+	}
+}
